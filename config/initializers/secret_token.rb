b56738681676060071876f80134ca44d8691dff9
Andy White <andy@wireworldmedia.co.uk>
2012-09-13 13:50:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pipeline::Application.config.secret_token = '1b19018836191e3257a91425174cb492449969f8eec03afa97aade97bb724860efb003865b4458a19b2548fa740133bcce8252539dde328487e46bdd23965c2e'
